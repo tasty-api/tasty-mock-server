be13f94dc0d889a27f2f4f5618394731afeaf64e
ylitvyakov <ylitvyakov@yahoo.com>
2019-07-04 13:15:07 +0300

    Add favicon to application

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.use('/', indexRouter);
 app.use('/documentation', documentationRouter);
